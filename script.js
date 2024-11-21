d4aeba7da11d9e7682819085cccbe88aeefe9bbd
Enrique <luisvalrc@gmail.com>
2024-11-20 19:46:21 -0500

    esi_1

---
+++ script.js
@@ -0,0 +1,35 @@
+document.getElementById('changeProductBtn').addEventListener('click', function() {
+    document.getElementById('selectionScreen').style.display = 'none';
+    document.querySelector('.change-product-section').style.display = 'flex';
+});
+
+document.getElementById('returnProductBtn').addEventListener('click', function() {
+    document.getElementById('selectionScreen').style.display = 'none';
+    document.querySelector('.return-product-section').style.display = 'flex';
+});
+
+document.getElementById('changeProductForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    showConfirmation('Cambio de Producto');
+});
+
+document.getElementById('returnProductForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    showConfirmation('Devolución de Producto');
+});
+
+function showConfirmation(type) {
+const supportCode = Math.floor(Math.random() * 10000); // Genera un código de atención aleatorio
+const date = new Date().toLocaleDateString();
+const name = type === 'Cambio de Producto' ? document.getElementById('changeFullName').value : document.getElementById('returnFullName').value;
+const email = type === 'Cambio de Producto' ? document.getElementById('changeEmail').value : document.getElementById('returnEmail').value;
+
+document.getElementById('supportCode').innerText = supportCode;
+document.getElementById('submissionDate').innerText = date;
+document.getElementById('submittedName').innerText = name;
+document.getElementById('submittedEmail').innerText = email;
+
+document.querySelector('.change-product-section').style.display = 'none';
+document.querySelector('.return-product-section').style.display = 'none';
+document.querySelector('.confirmation-section').style.display = 'flex';
+}
